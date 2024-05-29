6a20e38b7d40366d0623fb1ef3443ffdf4b2fae2
Alex_qp <alex-schmid11@web.de>
2024-05-29 13:10:26 +0200

    redo internals implementation to fix PaperMC

---
+++ unstriplog-core/src/main/java/com/github/alexqp/unstriplog/main/UnstripLog.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2018-2024 Alexander Schmid
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.alexqp.unstriplog.main;
+
+import com.github.alexqp.commons.bstats.bukkit.Metrics;
+import com.github.alexqp.commons.config.ConfigChecker;
+import com.github.alexqp.commons.config.ConsoleErrorType;
+import com.github.alexqp.commons.messages.ConsoleMessage;
+import com.github.alexqp.commons.messages.Debugable;
+import com.google.common.collect.Range;
+
+import com.jeff_media.updatechecker.UpdateCheckSource;
+import com.jeff_media.updatechecker.UpdateChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.java.JavaPlugin;
+import com.github.alexqp.unstriplog.listeners.GrassStripListener;
+import com.github.alexqp.unstriplog.listeners.LogStripListener;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+public class UnstripLog extends JavaPlugin implements Debugable {
+
+    @Override
+    public boolean getDebug() {
+        return false;
+    }
+
+    private static final String defaultInternalsVersion = "Internals_v1_20_6";
+    private static InternalsProvider internals;
+    static {
+        try {
+            String packageName = UnstripLog.class.getPackage().getName();
+            String minecraftVersion = Bukkit.getServer().getBukkitVersion().split("-")[0];
+            String internalsName = getInternalsName(minecraftVersion);
+            if (internalsName.equals(defaultInternalsVersion)) {
+                Bukkit.getLogger().log(Level.INFO, "UnstripLog is using the latest implementation (last tested for " + defaultInternalsVersion + ").");
+                internals = new InternalsProvider();
+            } else {
+                Bukkit.getLogger().log(Level.INFO, UnstripLog.class.getSimpleName() + " is using the implementation for version " + internalsName + ".");
+                internals = (InternalsProvider) Class.forName(packageName + "." + internalsName).getDeclaredConstructor().newInstance();
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException | NoSuchMethodException | InvocationTargetException exception) {
+            Bukkit.getLogger().log(Level.WARNING, UnstripLog.class.getSimpleName() + " could not find an updated implementation for this server version. " +
+                    "However the plugin is trying to use the latest implementation which should work if Minecraft did not change drastically (last tested version: " + defaultInternalsVersion + ").");
+            internals = new InternalsProvider();
+        }
+    }
+
+    private static String getInternalsName(String minecraftVersion) {
+        if (minecraftVersion.startsWith("1.13") || minecraftVersion.startsWith("1.14") || minecraftVersion.startsWith("1.15")) {
+            return "Internals_v1_15_2";
+        } else if (minecraftVersion.startsWith("1.16")) {
+            // added netherite tools, nether wood
+            return "Internals_v1_16_5";
+        } else if (minecraftVersion.startsWith("1.17") || minecraftVersion.startsWith("1.18")) {
+            // renamed GRASS_PATH to DIRT_PATH, added ROOTED_DIRT
+            return "Internals_v1_18_2";
+        } else if (minecraftVersion.startsWith("1.19")) {
+            // added mangrove wood
+            return "Internals_v1_19_4";
+        }
+        // added bamboo, cherry wood
+        return defaultInternalsVersion;
+    }
+
+    @Override
+    public void onEnable() {
+        new Metrics(this, 3642);
+        this.saveDefaultConfig();
+        this.getLogger().info("This plugin was made by alex_qp.");
+        this.updateChecker();
+
+        ConfigChecker configChecker = new ConfigChecker(this);
+        // be aware that values in (-1, 0) are not possible because value is integer!
+        int grassDelay = configChecker.checkInt(this.getConfig(), "timeframe_path", ConsoleErrorType.WARN, 100, Range.atLeast(-1));
+        int logDelay = configChecker.checkInt(this.getConfig(), "timeframe_wood", ConsoleErrorType.WARN, 100, Range.atLeast(-1));
+        boolean mustSneakUnstrip = configChecker.checkBoolean(this.getConfig(), "must_sneak_unstrip", ConsoleErrorType.WARN, true);
+        boolean mustSneakStrip = configChecker.checkBoolean(this.getConfig(), "must_sneak_strip", ConsoleErrorType.WARN, false);
+
+        boolean keepEnabled = false;
+
+
+        if (grassDelay != 0 || mustSneakStrip) {
+            Bukkit.getPluginManager().registerEvents(new GrassStripListener(this, internals, grassDelay, mustSneakUnstrip, mustSneakStrip), this);
+            if (grassDelay != 0)
+                this.getLogger().info("Unstripping of " + internals.getGrassStrippedType().name().toLowerCase() + " enabled with delay " + grassDelay);
+            keepEnabled = true;
+        }
+
+        if (logDelay != 0 || mustSneakStrip) {
+            Bukkit.getPluginManager().registerEvents(new LogStripListener(this, internals, logDelay, mustSneakUnstrip, mustSneakStrip), this);
+            if (logDelay != 0)
+                this.getLogger().info("Unstripping of stripped log/wood enabled with delay " + logDelay);
+            keepEnabled = true;
+        }
+
+        if (!keepEnabled)
+            this.onDisable();
+    }
+
+    private void updateChecker() {
+        int spigotResourceID = 62738;
+        ConfigChecker configChecker = new ConfigChecker(this);
+        ConfigurationSection updateCheckerSection = configChecker.checkConfigSection(this.getConfig(), "updatechecker", ConsoleErrorType.ERROR);
+        if (updateCheckerSection != null && configChecker.checkBoolean(updateCheckerSection, "enable", ConsoleErrorType.WARN, true)) {
+            ConsoleMessage.debug((Debugable) this, "enabled UpdateChecker");
+
+            new UpdateChecker(this, UpdateCheckSource.SPIGOT, String.valueOf(spigotResourceID))
+                    .setDownloadLink(spigotResourceID)
+                    .setChangelogLink("https://www.spigotmc.org/resources/" + spigotResourceID + "/updates")
+                    .setDonationLink("https://paypal.me/alexqpplugins")
+                    .setNotifyOpsOnJoin(configChecker.checkBoolean(updateCheckerSection, "notify_op_on_login", ConsoleErrorType.WARN, true))
+                    .setNotifyByPermissionOnJoin("betterconcrete.updatechecker")
+                    .checkEveryXHours(24).checkNow();
+        }
+    }
+}
---
+++ unstriplog-internals_1_18_2/src/main/java/com/github/alexqp/unstriplog/main/Internals_v1_18_2.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2018-2024 Alexander Schmid
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.alexqp.unstriplog.main;
+
+import org.bukkit.Material;
+
+public class Internals_v1_18_2 extends InternalsProvider {
+
+    protected void setGrassStrippedType() {
+        grassStrippedType = Material.DIRT_PATH;
+    }
+
+    protected void createGrassOriginBlockTypeSet() {
+        grassOriginBlockTypeSet.add(Material.GRASS_BLOCK);
+        grassOriginBlockTypeSet.add(Material.DIRT);
+        grassOriginBlockTypeSet.add(Material.COARSE_DIRT);
+        grassOriginBlockTypeSet.add(Material.MYCELIUM);
+        grassOriginBlockTypeSet.add(Material.PODZOL);
+        grassOriginBlockTypeSet.add(Material.ROOTED_DIRT);
+    }
+
+    protected void createGrassToolTypeSet() {
+        grassToolTypeSet.add(Material.DIAMOND_SHOVEL);
+        grassToolTypeSet.add(Material.GOLDEN_SHOVEL);
+        grassToolTypeSet.add(Material.IRON_SHOVEL);
+        grassToolTypeSet.add(Material.STONE_SHOVEL);
+        grassToolTypeSet.add(Material.WOODEN_SHOVEL);
+        grassToolTypeSet.add(Material.NETHERITE_SHOVEL);
+    }
+
+    protected void createLogOriginBlockTypeSet() {
+        logOriginBlockTypeSet.add(Material.ACACIA_LOG);
+        logOriginBlockTypeSet.add(Material.BIRCH_LOG);
+        logOriginBlockTypeSet.add(Material.DARK_OAK_LOG);
+        logOriginBlockTypeSet.add(Material.JUNGLE_LOG);
+        logOriginBlockTypeSet.add(Material.SPRUCE_LOG);
+        logOriginBlockTypeSet.add(Material.OAK_LOG);
+        logOriginBlockTypeSet.add(Material.ACACIA_WOOD);
+        logOriginBlockTypeSet.add(Material.BIRCH_WOOD);
+        logOriginBlockTypeSet.add(Material.DARK_OAK_WOOD);
+        logOriginBlockTypeSet.add(Material.JUNGLE_WOOD);
+        logOriginBlockTypeSet.add(Material.SPRUCE_WOOD);
+        logOriginBlockTypeSet.add(Material.OAK_WOOD);
+        logOriginBlockTypeSet.add(Material.WARPED_STEM);
+        logOriginBlockTypeSet.add(Material.WARPED_HYPHAE);
+        logOriginBlockTypeSet.add(Material.CRIMSON_STEM);
+        logOriginBlockTypeSet.add(Material.CRIMSON_HYPHAE);
+    }
+
+    protected void createLogToolTypeSet() {
+        logToolTypeSet.add(Material.DIAMOND_AXE);
+        logToolTypeSet.add(Material.GOLDEN_AXE);
+        logToolTypeSet.add(Material.IRON_AXE);
+        logToolTypeSet.add(Material.STONE_AXE);
+        logToolTypeSet.add(Material.WOODEN_AXE);
+        logToolTypeSet.add(Material.NETHERITE_AXE);
+    }
+}
